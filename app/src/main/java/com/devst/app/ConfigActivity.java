2f8bd8e4ab6d7dd2e6b1807d5a99444f9eaf9e6b
MCKAYDELIMON <indo.nicolas@gmail.com>
2025-10-24 01:10:03 -0300

    Sms

--- app/src/main/java/com/devst/app/ConfigActivity.java
+++ app/src/main/java/com/devst/app/ConfigActivity.java
@@ -1,6 +1,11 @@
 package com.devst.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -11,6 +16,7 @@ public class ConfigActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
 
+        TextView tvEnviarSms = findViewById(R.id.tvEnviarSms);
         Toolbar toolbar = findViewById(R.id.toolbarConfig);
         setSupportActionBar(toolbar);
 
@@ -18,6 +24,20 @@ public class ConfigActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        tvEnviarSms.setOnClickListener(v -> {
+            String numeroTelefonico = "+56942137877";
+
+            Intent intentSms = new Intent(Intent.ACTION_SENDTO);
+            intentSms.setData(Uri.parse("smsto:" + numeroTelefonico));
+            intentSms.putExtra("smsbody", "Hola, necesito ayuda con la aplicación.");
+
+            if (intentSms.resolveActivity(getPackageManager()) != null){
+                startActivity(intentSms);
+            }else{
+                Toast.makeText(this,"No se encontró una aplicación de mensajería.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
