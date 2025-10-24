6c9cbc7969a900aefdefa4ea2f435785008f9047
SamaelBlossom <samaelblossom.py@mail.com>
2025-10-24 09:43:57 -0300

    cambio: version minima de android: 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.devst.app"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.devst.app"
         minSdk = 31
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
